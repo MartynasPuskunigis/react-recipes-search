92f6e97013615324bb744676f0585273e262c8c3
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-07-24 17:24:21 +0300

    Fixed things from pull request #10.

--- src/app/components/router/router.tsx
+++ src/app/components/router/router.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import { HomeView } from "../home/home-view";
+import { ContentLayoutView } from "./content-layout";
 import { HeaderView } from "../header/header-view";
 import { FooterView } from "../footer/footer-view";
-import { FavRecipesView } from "../fav-recipe/fav-recipes-view";
-import { ActiveRecipeContainer } from "../../containers/active-recipe-container";
 
 import "./router.css";
 
@@ -13,15 +11,9 @@ export class Router extends React.Component {
     public render(): JSX.Element | JSX.Element[] {
         return (
             <BrowserRouter>
-                <div className="router">
+                <div className="page-layout">
                     <HeaderView />
-                    <div className="content">
-                        <Switch>
-                            <Route path="/" component={HomeView} exact />
-                            <Route path="/recipe/:id" component={ActiveRecipeContainer} />
-                            <Route path="/favorites" component={FavRecipesView} />
-                        </Switch>
-                    </div>
+                    <ContentLayoutView />
                     <FooterView />
                 </div>
             </BrowserRouter>
