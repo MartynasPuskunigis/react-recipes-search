d8bc091f0c3b82fc2696914acb22339c836f3200
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-07-24 16:52:19 +0300

    Added stuff that got deleted in conflict resolving.

--- src/app/components/recipe/recipes-item-view.tsx
+++ src/app/components/recipe/recipes-item-view.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactTooltip from "react-tooltip";
+import * as classNames from "classnames";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 
@@ -42,17 +43,19 @@ export class RecipesItemView extends React.Component<Props> {
                     <p className="recipes-subtitle">
                         Publisher: <span>{this.props.recipe.publisher}</span>
                     </p>
-                    {this.props.isFavorite !== true ? (
-                        <div
-                            className="far fa-star star-icon-empty"
-                            onClick={event => this.handleFavoriteClick(event, this.props.recipe.recipe_id)}
-                        />
-                    ) : (
-                        <div
-                            className="fas fa-star star-icon-full"
-                            onClick={event => this.handleFavoriteClick(event, this.props.recipe.recipe_id)}
-                        />
-                    )}
+                    <div
+                        className={classNames(
+                            {
+                                "far fa-star": !this.props.isFavorite,
+                                "fas fa-star": this.props.isFavorite
+                            },
+                            {
+                                "star-icon-empty": !this.props.isFavorite,
+                                "star-icon-full": this.props.isFavorite
+                            }
+                        )}
+                        onClick={event => this.handleFavoriteClick(event, this.props.recipe.recipe_id)}
+                    />
                 </div>
                 <Link to={`/recipe/${this.props.recipe.recipe_id}`} className="recipe-buttons">
                     View Recipe
