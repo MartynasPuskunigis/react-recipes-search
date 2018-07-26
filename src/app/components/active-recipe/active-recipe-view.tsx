ae3792a6c42229c12f2db499f2053929995d6d71
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-07-26 12:28:53 +0300

    Active recipe styled.

--- src/app/components/active-recipe/active-recipe-view.tsx
+++ src/app/components/active-recipe/active-recipe-view.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 import { Recipe } from "../../contracts/Recipe";
 
@@ -22,28 +22,31 @@ export class ActiveRecipeView extends React.Component<Props> {
                             alt={this.props.recipeToDisplay.title}
                         />
                         <div className="recipe-details">
-                            <div className="ingredients-box">
-                                <div className="ingredients-list-header">The ingredients you will need</div>
-                                <div className="ingredients-list">
-                                    {this.props.recipeToDisplay.ingredients.map((ingredient, index) => (
-                                        <div key={`ingredient-item-${ingredient}-${index}`}>{ingredient}</div>
-                                    ))}
+                            <div className="wrapper">
+                                <div className="ingredients-box">
+                                    <div className="ingredients-list-header">The ingredients you will need</div>
+                                    <div className="ingredients-list">
+                                        {this.props.recipeToDisplay.ingredients.map((ingredient, index) => (
+                                            <div key={`ingredient-item-${ingredient}-${index}`}>{ingredient}</div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="recipe-info">
-                                <h4 className="active-recipe-publisher">
-                                    Publisher: <span>{this.props.recipeToDisplay.publisher}</span>
-                                </h4>
-                                <p className="active-recipe-website">
-                                    Website:
-                                    <span>
-                                        <a href={this.props.recipeToDisplay.source_url}>"Check out recipe here!"</a>
-                                    </span>
-                                </p>
-
-                                <button className="active-recipe-button">
-                                    <Link to="/">Go Home</Link>
-                                </button>
+                                <div className="recipe-info">
+                                    <div className="wrapper">
+                                        <div className="active-recipe-publisher">
+                                            Publisher: <span>{this.props.recipeToDisplay.publisher}</span>
+                                        </div>
+                                        <div className="active-recipe-website">
+                                            Website:
+                                            <span>
+                                                <a href={this.props.recipeToDisplay.source_url}> Check out recipe here!</a>
+                                            </span>
+                                        </div>
+                                        {/* <Link className="active-recipe-button" to="/">
+                                        Go Home
+                                    </Link> */}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
